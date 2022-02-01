bb7a11806b5e0a8c62b15a86387932acc29a517b
BSV17 <seregars17@gmail.com>
2022-02-01 14:31:43 +0200

    added options for for using console method

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.info("Hi there!");
+console.time("Reaction time");
+alert("Read this and press OK ");
+console.timeEnd("Reaction time");
+console.warn("Be careful");
+console.error("This is fatal error!");
